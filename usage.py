7c84926efd22619e6396ba00d07a3b7dd00fc7fa
faklijta <faklijta@gmail.com>
2017-09-28 21:22:19 +0200

    fix(todo app) user info layout correct

--- usage.py
+++ usage.py
@@ -1,10 +1,11 @@
 class Usage(object):
-    
+
     def usage_info(self):
-        print("Command Line Todo applications\n\
+        print(str("\n\
+        Command Line Todo applications\n\
         =============================\n\
         Command line arguments:\n\
         -l   Lists all the tasks\n\
         -a   Adds a new task\n\
         -r   Removes an task\n\
-        -c   Completes an task")
+        -c   Completes an task"))
