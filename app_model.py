4bd8eb32218f5356226ab46eaf81af79e020e044
faklijta <faklijta@gmail.com>
2017-10-01 15:56:48 +0200

    feat(todo app) remove action working

--- app_model.py
+++ app_model.py
@@ -18,9 +18,16 @@ class ToDoModel(object):
             self.file.write("0" + " " + todo + "\n" )
     
     def remove(self, index):
-        with open("todos.txt", "w") as self.file:
-            self.todo_list.remove(self.todo_list[int(index)-1])
-            self.file.write("0" + " " + todo + "\n" )
+            f = open("todos.txt", 'r')
+            listed = list(f)
+            del listed[int(index)-1]
+            f = open("todos.txt", 'w')
+            for line in listed:
+                f.write(line)
+            f.close()
+        # with open("todos.txt", "w") as self.file:
+        #     self.todo_list.remove(self.todo_list[int(index)-1])
+        #     self.file.write("0" + " " + todo + "\n" )
     
     def status(self, status):
         self.todo_list[int(index)-1]["checked"] = True
